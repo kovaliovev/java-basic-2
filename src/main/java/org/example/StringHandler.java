f7d63c821299ad28b61a5497bb178df82152f9fb
Yevhenii Kovalov <kovaliov.evgeniy11@gmail.com>
2024-10-13 22:52:46 +0300

    Implement removing specific words from string

--- src/main/java/org/example/StringHandler.java
+++ src/main/java/org/example/StringHandler.java
@@ -1,3 +1,27 @@
 package org.example;
+import java.util.StringTokenizer;
 
-public class StringHandler { }
+public class StringHandler {
+
+    public static StringBuffer removeWords(String text, int lengthToRemove) {
+
+        StringBuffer result = new StringBuffer();
+
+        String vowels = "AEIOUYaeiouy";
+
+        StringTokenizer tokenizer = new StringTokenizer(text);
+
+        while (tokenizer.hasMoreTokens()) {
+            String word = tokenizer.nextToken();
+
+            if (word.length() == lengthToRemove && vowels.indexOf(word.charAt(0)) == -1) {
+                continue;
+            }
+
+            result.append(word).append(" ");
+        }
+
+        if (!result.isEmpty()) result.deleteCharAt(result.length() - 1);
+        return result;
+    }
+}
